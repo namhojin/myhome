3d4870058738907736ff0a9f43b8dfc771bf0608
namhojin <hojnam23@gmail.com>
2017-08-17 17:56:11 +0900

    add today

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,8 +11,6 @@
 <link rel="stylesheet" type="text/css" href="style.css?ver=1">
 
 
-
-
 <link href='fullcalendar-3.4.0/fullcalendar.min.css' rel='stylesheet' />
 <link href='fullcalendar-3.4.0/fullcalendar.print.min.css' rel='stylesheet' media='print' />
 <script src='fullcalendar-3.4.0/lib/moment.min.js'></script>
@@ -24,69 +20,69 @@
 
 	$(document).ready(function() {
 
-		$('#calendar').fullCalendar({
-			defaultDate: '2017-05-12',
-			editable: true,
-			eventLimit: true, // allow "more" link when too many events
-			events: [
-				{
-					title: 'All Day Event',
-					start: '2017-05-01'
-				},
-				{
-					title: 'Long Event',
-					start: '2017-05-07',
-					end: '2017-05-10'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2017-05-09T16:00:00'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2017-05-16T16:00:00'
-				},
-				{
-					title: 'Conference',
-					start: '2017-05-11',
-					end: '2017-05-13'
-				},
-				{
-					title: 'Meeting',
-					start: '2017-05-12T10:30:00',
-					end: '2017-05-12T12:30:00'
-				},
-				{
-					title: 'Lunch',
-					start: '2017-05-12T12:00:00'
-				},
-				{
-					title: 'Meeting',
-					start: '2017-05-12T14:30:00'
-				},
-				{
-					title: 'Happy Hour',
-					start: '2017-05-12T17:30:00'
-				},
-				{
-					title: 'Dinner',
-					start: '2017-05-12T20:00:00'
-				},
-				{
-					title: 'Birthday Party',
-					start: '2017-05-13T07:00:00'
-				},
-				{
-					title: 'Click for Google',
-					url: 'http://google.com/',
-					start: '2017-05-28'
-				}
-			]
+    $('#calendar').fullCalendar({
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2017-05-01'
+        },
+        {
+          title: 'Long Event',
+          start: '2017-05-07',
+          end: '2017-05-10'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          start: '2017-05-09T16:00:00'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          start: '2017-05-16T16:00:00'
+        },
+        {
+          title: 'Conference',
+          start: '2017-05-11',
+          end: '2017-05-13'
+        },
+        {
+          title: 'Meeting',
+          start: '2017-05-12T10:30:00',
+          end: '2017-05-12T12:30:00'
+        },
+        {
+          title: 'Lunch',
+          start: '2017-05-12T12:00:00'
+        },
+        {
+          title: 'Meeting',
+          start: '2017-05-12T14:30:00'
+        },
+        {
+          title: 'Happy Hour',
+          start: '2017-05-12T17:30:00'
+        },
+        {
+          title: 'Dinner',
+          start: '2017-05-12T20:00:00'
+        },
+        {
+          title: 'Birthday Party',
+          start: '2017-05-13T07:00:00'
+        },
+        {
+          title: 'Click for Google',
+          url: 'http://google.com/',
+          start: '2017-05-28'
+        }
+      ]
 
-		});
 
+
+      });
 
 
 	});
